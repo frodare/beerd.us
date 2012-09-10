87b7d72f8eb682f788f7d861b9f11d5537a4dc23
Charles Howard <frodare@gmail.com>
2012-09-10 11:28:12 -0400

    removing howtonode references

--- server/server.js
+++ server/server.js
@@ -5,5 +5,5 @@ var Stack = require('stack'),
 
 Http.createServer(Stack(
   Creationix.log(),
-  require('wheat')(process.env.JOYENT ? process.env.HOME + "/howtonode" : __dirname +"/..")
+  require('wheat')(process.env.JOYENT ? process.env.HOME + "/beerd.us" : __dirname +"/..")
 )).listen(9000);
